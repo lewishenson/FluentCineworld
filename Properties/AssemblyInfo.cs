424fb0b2429996c8a4e814128d964a973eafb114
lewishenson <FW6OoagolpGqEiTUQaH9xBg3OC5vF6>
2014-01-26 00:47:58 +0000

    Updated assembly info with new version number.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LewisHenson.CineworldCinemas")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# API for Cineworld")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lewis Henson")]
 [assembly: AssemblyProduct("LewisHenson.CineworldCinemas")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Lewis Henson 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
