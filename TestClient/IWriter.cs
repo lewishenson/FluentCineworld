e491641a3a1607ecbe96134cee29bb3ff02fe832
lewishenson <FW6OoagolpGqEiTUQaH9xBg3OC5vF6>
2014-02-22 22:00:19 +0000

    Added test client.

---
+++ TestClient/IWriter.cs
@@ -0,0 +1,10 @@
+﻿using FluentCineworld.Listings;
+using System.Collections.Generic;
+
+namespace FluentCineworld.TestClient
+{
+    public interface IWriter
+    {
+        void Output(IEnumerable<Film> films);
+    }
+}
