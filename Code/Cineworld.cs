519465603b2a4762d5f29b4b4d7a2fb57926db44
Lewis Henson <lewishenson@users.noreply.github.com>
2015-09-12 21:33:37 +0100

    Remove obsolete code (old functionality).

--- Code/Cineworld.cs
+++ Code/Cineworld.cs
@@ -1,24 +1,10 @@
-﻿using System;
-using FluentCineworld.Details;
-using FluentCineworld.Listings;
+﻿using FluentCineworld.Listings;
 using FluentCineworld.Sites;
 
 namespace FluentCineworld
 {
     public static class Cineworld
     {
-        [Obsolete]
-        public static OldListings.ICineworldListings WhatsOnOld(Cinema cinema)
-        {
-            return new OldListings.CineworldListings(cinema);
-        }
-
-        [Obsolete]
-        public static CinemaDetails Details(Cinema cinema)
-        {
-            return new CineworldDetails(cinema).Retreive();
-        }
-
         public static SiteDetails Site(Cinema cinema)
         {
             return new SiteDetailsQueryExecutor(cinema).Execute();
