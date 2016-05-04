1df09ca9d98d0cf59b032b488ff70618efe3c378
Lewis Henson <lewishenson@users.noreply.github.com>
2016-05-04 21:00:24 +0100

    Update cinema list.

--- Code/Cinema.cs
+++ Code/Cinema.cs
@@ -1,14 +1,19 @@
-﻿namespace FluentCineworld
+﻿using System;
+
+namespace FluentCineworld
 {
     public class Cinema : Enumeration
     {
         public static readonly Cinema AberdeenQueensLinks = new Cinema(1010804, "Aberdeen - Queens Links");
+        public static readonly Cinema AberdeenUnionSquare = new Cinema(1010808, "Aberdeen - Union Square");
+        [Obsolete("Use AberdeenUnionSquare instead - AberdeenUnionSquire will be removed in a future version.")]
         public static readonly Cinema AberdeenUnionSquire = new Cinema(1010808, "Aberdeen - Union Square");
         public static readonly Cinema Aldershot = new Cinema(1010805, "Aldershot");
         public static readonly Cinema Ashford = new Cinema(1010806, "Ashford");
         public static readonly Cinema AshtonUnderLyne = new Cinema(1010807, "Ashton-under-Lyne");
         public static readonly Cinema Bedford = new Cinema(1010809, "Bedford");
         public static readonly Cinema BirminghamBroadStreet = new Cinema(1010812, "Birmingham - Broad Street");
+        public static readonly Cinema BirminghamNEC = new Cinema(1010892, "Birmingham - NEC");
         public static readonly Cinema BoldonTyneAndWear = new Cinema(1010819, "Boldon Tyne and Wear");
         public static readonly Cinema Bolton = new Cinema(1010813, "Bolton");
         public static readonly Cinema Bradford = new Cinema(1010811, "Bradford");
@@ -38,6 +43,7 @@
         public static readonly Cinema Harlow = new Cinema(1010846, "Harlow");
         public static readonly Cinema Haverhill = new Cinema(1010847, "Haverhill");
         public static readonly Cinema HighWycombe = new Cinema(1010851, "High Wycombe");
+        public static readonly Cinema Hinckley = new Cinema(1010895, "Hinckley");
         public static readonly Cinema Hull = new Cinema(1010849, "Hull");
         public static readonly Cinema Huntingdon = new Cinema(1010850, "Huntingdon");
         public static readonly Cinema Ipswich = new Cinema(1010854, "Ipswich");
@@ -51,6 +57,7 @@
         public static readonly Cinema LondonEnfield = new Cinema(1010835, "London - Enfield");
         public static readonly Cinema LondonFeltham = new Cinema(1010837, "London - Feltham");
         public static readonly Cinema LondonFulhamRoad = new Cinema(1010838, "London - Fulham Road");
+        [Obsolete("Cinema has closed - LondonHammersmith will be removed in a future version.")]
         public static readonly Cinema LondonHammersmith = new Cinema(1010845, "London - Hammersmith");
         public static readonly Cinema LondonHaymarket = new Cinema(1010848, "London - Haymarket");
         public static readonly Cinema LondonIlford = new Cinema(1010852, "London - Ilford");
@@ -60,15 +67,20 @@
         public static readonly Cinema LondonWembley = new Cinema(1010880, "London - Wembley");
         public static readonly Cinema LondonWestIndiaQuay = new Cinema(1010882, "London - West India Quay");
         public static readonly Cinema LondonWoodGreen = new Cinema(1010884, "London - Wood Green");
+        public static readonly Cinema Loughborough = new Cinema(1010898, "Loughborough");
         public static readonly Cinema Luton = new Cinema(1010859, "Luton");
         public static readonly Cinema Middlesbrough = new Cinema(1010860, "Middlesbrough");
         public static readonly Cinema MiltonKeynes = new Cinema(1010861, "Milton Keynes");
+        [Obsolete("Use NewportSpyttyPark instead - Newport will be removed in a future version.")]
         public static readonly Cinema Newport = new Cinema(1010862, "Newport - Spytty Park");
+        public static readonly Cinema NewportFriarsWalk = new Cinema(1010893, "Newport - Friars Walk");
+        public static readonly Cinema NewportSpyttyPark = new Cinema(1010862, "Newport - Spytty Park");
         public static readonly Cinema Northampton = new Cinema(1010863, "Northampton");
         public static readonly Cinema Nottingham = new Cinema(1010864, "Nottingham");
         public static readonly Cinema Rochester = new Cinema(1010865, "Rochester");
         public static readonly Cinema Rugby = new Cinema(1010866, "Rugby");
         public static readonly Cinema Runcorn = new Cinema(1010867, "Runcorn");
+        public static readonly Cinema TheScreeningRoomsCheltenham = new Cinema(1010828, "The Screening Rooms - Cheltenham");
         public static readonly Cinema Sheffield = new Cinema(1010869, "Sheffield");
         public static readonly Cinema Shrewsbury = new Cinema(1010870, "Shrewsbury");
         public static readonly Cinema Solihull = new Cinema(1010871, "Solihull");
@@ -77,14 +89,16 @@
         public static readonly Cinema StNeots = new Cinema(1010887, "St Neots");
         public static readonly Cinema Stevenage = new Cinema(1010874, "Stevenage");
         public static readonly Cinema Stockport = new Cinema(1010876, "Stockport");
+        public static readonly Cinema StokeOnTrent = new Cinema(1010896, "Stoke-on-Trent");
         public static readonly Cinema SwindonRegentCircus = new Cinema(1010888, "Swindon - Regent Circus");
         public static readonly Cinema SwindonShawRidge = new Cinema(1010877, "Swindon - Shaw Ridge");
         public static readonly Cinema Telford = new Cinema(1010802, "Telford");
-        public static readonly Cinema TheScreeningRoomsCheltenham = new Cinema(1010828, "The Screening Rooms - Cheltenham");
         public static readonly Cinema Wakefield = new Cinema(1010878, "Wakefield");
         public static readonly Cinema Weymouth = new Cinema(1010881, "Weymouth");
+        public static readonly Cinema Whiteley = new Cinema(1010894, "Whiteley");
         public static readonly Cinema Witney = new Cinema(1010883, "Witney");
         public static readonly Cinema Wolverhampton = new Cinema(1010885, "Wolverhampton");
+        public static readonly Cinema Yate = new Cinema(1010897, "Yate");
         public static readonly Cinema Yeovil = new Cinema(1010886, "Yeovil");
 
         protected Cinema()
