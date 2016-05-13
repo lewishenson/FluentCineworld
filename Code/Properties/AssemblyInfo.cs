f3ff94741008a5a191809589960a0f9c1f6b5264
Lewis Henson <lewis.henson@gmail.com>
2016-05-13 21:23:37 +0100

    Incremented version number to 2.1.0.0.

--- Code/Properties/AssemblyInfo.cs
+++ Code/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lewis Henson")]
 [assembly: AssemblyProduct("FluentCineworld")]
-[assembly: AssemblyCopyright("Copyright © Lewis Henson 2015")]
+[assembly: AssemblyCopyright("Copyright © Lewis Henson 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
