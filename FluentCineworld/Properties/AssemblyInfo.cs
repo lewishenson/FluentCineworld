477ba79362d7b4d7d660ac76f34b003b3e560885
Lewis Henson <lewishenson@users.noreply.github.com>
2017-04-12 22:54:33 +0100

    Update version number and changelog to 3.1.0.

--- FluentCineworld/Properties/AssemblyInfo.cs
+++ FluentCineworld/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lewis Henson")]
 [assembly: AssemblyProduct("FluentCineworld")]
-[assembly: AssemblyCopyright("Copyright © Lewis Henson 2016")]
+[assembly: AssemblyCopyright("Copyright © Lewis Henson 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
