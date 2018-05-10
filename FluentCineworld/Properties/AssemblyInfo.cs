1f69166769b17c58c98fe6018bb6a2c3a464b759
Lewis Henson <lewishenson@users.noreply.github.com>
2018-05-10 23:24:04 +0100

    Increment version number to 4.0.1.

--- FluentCineworld/Properties/AssemblyInfo.cs
+++ FluentCineworld/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lewis Henson")]
 [assembly: AssemblyProduct("FluentCineworld")]
-[assembly: AssemblyCopyright("Copyright © Lewis Henson 2017")]
+[assembly: AssemblyCopyright("Copyright © Lewis Henson 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.0.1.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
