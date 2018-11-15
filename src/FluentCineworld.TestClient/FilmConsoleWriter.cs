dfbc19af162dce6572c58e741dea0fd80f0430c1
Lewis Henson <lewishenson@users.noreply.github.com>
2018-11-15 21:48:25 +0000

    Include showings when getting listings

--- src/FluentCineworld.TestClient/FilmConsoleWriter.cs
+++ src/FluentCineworld.TestClient/FilmConsoleWriter.cs
@@ -13,8 +13,32 @@ namespace FluentCineworld.TestClient
 
             foreach (var film in films)
             {
-                Console.WriteLine($"> {film.Name}");
+                this.Output(film);
             }
         }
+
+        private void Output(Film film)
+        {
+            Console.WriteLine("> {0} ({1})", film.Name, film.Rating);
+
+            foreach (var day in film.Days)
+            {
+                this.Output(day);
+            }
+        }
+
+        private void Output(Day day)
+        {
+            Console.Write("  {0} : ", day.Date.ToString("d"));
+
+            foreach (var show in day.Showings)
+            {
+                Console.Write(show.DisplayText);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+
     }
 }
