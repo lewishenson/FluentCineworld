95b8d579e3344332c4560d7392b06e16cee70790
Lewis Henson <lewis.henson@gmail.com>
2019-04-09 22:03:23 +0100

    Closes #15 by making FilmNameFormatter case insensitive.

---
+++ src/FluentCineworld.Tests/StringExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace FluentCineworld.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("Toy Story", "Story", "Film", "Toy Film")]
+        [InlineData("The LEGO Movie", "Duplo", "Bricks", "The LEGO Movie")]
+        [InlineData("The Lion, The Witch & The Wardrobe", "the", "A", "A Lion, A Witch & A Wardrobe")]
+        public void Replace_ReplacesStringAsExpected(string input, string oldValue, string newValue, string expectedOutput)
+        {
+            var output = input.Replace(oldValue, newValue, StringComparison.OrdinalIgnoreCase);
+
+            output.Should().Be(expectedOutput);
+        }
+    }
+}
