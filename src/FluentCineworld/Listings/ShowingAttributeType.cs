8edc582e858aabeba6eaf141d4ec739f8954a5b1
Lewis Henson <lewishenson@users.noreply.github.com>
2026-03-26 18:13:29 +0000

    Refresh showing attributes

--- src/FluentCineworld/Listings/ShowingAttributeType.cs
+++ src/FluentCineworld/Listings/ShowingAttributeType.cs
@@ -2,8 +2,13 @@ namespace FluentCineworld.Listings
 {
     public enum ShowingAttributeType
     {
-        AgeRestriction,
-        Extra,
-        ScreeningType,
+        Unknown = 0,
+        Rating = 1,
+        Format = 2,
+        Screening = 3,
+        Accessibility = 4,
+        Seating = 5,
+        Genre = 6,
+        Language = 7,
     }
 }
